52be8e7d15012badb39b3fc47f8363718bf74d2a
Joseph Hickey <joseph.p.hickey@nasa.gov>
2022-10-31 13:40:44 -0400

    Add FM and tests

--- common-test.mk
+++ common-test.mk
@@ -78,6 +78,13 @@ ALL_CFE_COVERAGE_TESTS += $(ALL_BPLIB_COVERAGE_TESTS)
 ALL_BP_COVERAGE_TESTS += coverage-bp-ALL-testrunner
 #ALL_CFE_COVERAGE_TESTS += $(ALL_BP_COVERAGE_TESTS)
 
+ALL_FM_COVERAGE_TESTS += coverage-fm-fm_app-testrunner
+ALL_FM_COVERAGE_TESTS += coverage-fm-fm_child-testrunner
+ALL_FM_COVERAGE_TESTS += coverage-fm-fm_cmds-testrunner
+ALL_FM_COVERAGE_TESTS += coverage-fm-fm_cmd_utils-testrunner
+ALL_FM_COVERAGE_TESTS += coverage-fm-fm_tbl-testrunner
+ALL_CFE_COVERAGE_TESTS += $(ALL_FM_COVERAGE_TESTS)
+
 
 ALL_OSAL_COVERAGE_TESTS :=                      \
 	coverage-shared-binsem-testrunner			\
--- sample_defs.noneds-draco/targets.cmake
+++ sample_defs.noneds-draco/targets.cmake
@@ -101,18 +101,19 @@ SET(FT_INSTALL_SUBDIR "host/functional-test")
 # Each target board can have its own HW arch selection and set of included apps
 SET(MISSION_CPUNAMES
     cpu1
-    cpu2
+#    cpu2
 )
 
 SET(cpu1_PROCESSORID 1)
 SET(cpu1_APPLIST cfe_assert)
 #SET(cpu1_APPLIST ci_lab to_lab sch_lab)
 SET(cpu1_APPLIST cfe_assert ci_lab to_lab sch_lab cf sgw)
-SET(cpu1_STATIC_APPLIST sample_app sample_lib bp bplib)
+SET(cpu1_STATIC_APPLIST sample_app sample_lib bp bplib fm)
 SET(cpu1_PSP_MODULELIST iodriver unsock_intf)
 SET(cpu1_FILELIST cfe_es_startup.scr)
 
 SET(cpu1_STATIC_SYMLIST
+    "FM_AppMain,FM"
     "BP_AppMain,BP_APP"
     "SAMPLE_LIB_Init,SAMPLE_LIB"
     "SAMPLE_APP_Main,SAMPLE_APP")
