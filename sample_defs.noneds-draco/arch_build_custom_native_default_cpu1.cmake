dddbf3ce22f5c93150796a201228f311dcdd44a4
Joseph Hickey <joseph.p.hickey@nasa.gov>
2023-01-16 17:28:39 -0500

    Add DS and HK modules, also add -Wno-restrict

--- sample_defs.noneds-draco/arch_build_custom_native_default_cpu1.cmake
+++ sample_defs.noneds-draco/arch_build_custom_native_default_cpu1.cmake
@@ -5,11 +5,12 @@
 # On native builds only, add strict cast alignment warnings
 # This requires a newer version of gcc
 #
+
 add_compile_options(
     -Wcast-align=strict         # Warn about casts that increase alignment requirements
     -fno-common                 # Do not use a common section for globals
     -Wno-conversion            # Lots of false positives
     -Wno-stringop-truncation   # Lots of false positives
     -Wno-format-truncation     # Lots of false positives
+    -Wno-restrict              # Mysterious error in OSAL
 )
-
