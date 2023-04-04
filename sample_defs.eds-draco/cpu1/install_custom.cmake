7f87acc016242db266bb3d95ca5895ab5d66712d
Joseph Hickey <joseph.p.hickey@nasa.gov>
2023-04-04 17:06:36 -0400

    Updates to enable HS app in EDS build

--- sample_defs.eds-draco/cpu1/install_custom.cmake
+++ sample_defs.eds-draco/cpu1/install_custom.cmake
@@ -1,4 +1,11 @@
-install(FILES /usr/lib/x86_64-linux-gnu/liblua5.3.so.0.0.0 
-	DESTINATION ${TGTNAME}/${INSTALL_SUBDIR} 
+install(FILES /usr/lib/x86_64-linux-gnu/liblua5.3.so.0.0.0
+	DESTINATION ${TGTNAME}/${INSTALL_SUBDIR}
 	RENAME liblua.so
 )
+
+#target_include_directories(cpu1_sch_lab_table_sch_lab_table PRIVATE
+#    $<TARGET_PROPERTY:hs,INCLUDE_DIRECTORIES>
+#)
+
+add_cfe_tables(to_lab to_lab_sub_custom.c)
+add_cfe_tables(sch_lab sch_lab_table_custom.c)
