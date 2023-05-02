cd598ff1914fa8f812a86358b88645e7d5d20060
Joseph Hickey <joseph.p.hickey@nasa.gov>
2023-05-02 10:54:08 -0400

    Setup noneds build for sample_app table test

--- sample_defs.noneds-draco/cpu1/install_custom.cmake
+++ sample_defs.noneds-draco/cpu1/install_custom.cmake
@@ -1,17 +1,17 @@
 
 message("inside install_custom cpu1")
-target_include_directories(tblobj_cpu1_sch_lab PRIVATE
+target_include_directories(sch_lab.table INTERFACE
     $<TARGET_PROPERTY:cf,INCLUDE_DIRECTORIES>
     $<TARGET_PROPERTY:sc,INCLUDE_DIRECTORIES>
 )
 
-if (TARGET tblobj_cpu1_bp)
-target_include_directories(tblobj_cpu1_sch_lab PRIVATE
+if (TARGET bp)
+target_include_directories(sch_lab.table INTERFACE
     $<TARGET_PROPERTY:bp,INCLUDE_DIRECTORIES>
 )
-target_include_directories(tblobj_cpu1_bp PRIVATE
+target_include_directories(bp.table INTERFACE
     $<TARGET_PROPERTY:cf,INCLUDE_DIRECTORIES>
 )
 endif()
 
-add_cfe_tables(sample_app alt_sample_tbl.c alt2_sample_tbl.c)
+add_cfe_tables(sample_app alt_sample_tbl.c alt2_sample_tbl2.c)
