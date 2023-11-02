4da95654ec929517e107a60d81b3e35b466c9d76
Joseph Hickey <joseph.p.hickey@nasa.gov>
2023-11-02 09:53:14 -0400

    Add needed lighttpd config

--- sample_defs.eds-draco/cpu1/install_custom.cmake
+++ sample_defs.eds-draco/cpu1/install_custom.cmake
@@ -3,5 +3,9 @@ install(FILES /usr/lib/x86_64-linux-gnu/liblua5.3.so.0.0.0
 	RENAME liblua.so
 )
 
+INSTALL(SCRIPT ${MISSION_SOURCE_DIR}/sample_defs.eds-draco/ui/gen-lighttpd-run.cmake)
+INSTALL(FILES ${MISSION_SOURCE_DIR}/sample_defs.eds-draco/ui/lighttpd.conf DESTINATION ${UI_INSTALL_SUBDIR})
+
+
 add_cfe_tables(to_lab tables/to_lab_sub_custom.c)
 add_cfe_tables(sch_lab tables/sch_lab_table_custom.c)
