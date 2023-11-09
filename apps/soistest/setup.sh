2ecb26114e4da88ff88c457dc98a6fa5018fc5df
Joseph Hickey <joseph.p.hickey@nasa.gov>
2023-11-09 10:50:14 -0500

    Update soistest

--- apps/soistest/setup.sh
+++ apps/soistest/setup.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+if [ ! -d ccsds.sois ]; then
+    exit 1
+fi
+
+mkdir -f eds
 cd eds || exit 1
-rm -f ./*.xml
+
+# leave the cleanup to manual step for now
+# rm -f ./*.xml
 
 find ../ccsds.sois/seds/interoperability/common/${1}/ -type f -name '*.xml' | while read -r i
 do
